049ee34b0e3fdee434a212d3db76a8c333dd8cdc
LiberalOtaku <ledgam3r1279@gmail.com>
2016-04-19 15:50:31 -0400

    serial implementation for is_match
    one target string, find number of matches

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// File: main.c
+// Authors: Edward Ly, Byron Roosa(, and possibly George Crowson)
+// Last Updated: 19 April 2016
+// Single-linkage clustering program on strings using MPI implementation.
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -34,7 +38,7 @@ int main(int argc, char *argv)
 	
 	// Main variables
 	int string_len, max_num_strings = 1e6, num_strings = 0, threshold = 2;
-	int diff, distance, local_distance, num_comps;
+	int diff, num_matches, local_distance, num_comps;
 	char *a, *local_a, *stream;
 	FILE *fstream = NULL;
 	
@@ -55,6 +59,83 @@ int main(int argc, char *argv)
 	size_t size = sizeof(char) * max_num_strings * array_len;
 	stream = (char *)malloc(sizeof(char) * array_len);
 	char target[array_len], other[array_len];
+	
+	// Start clock
+	start = MPI_Wtime();
+	
+	// Open file
+	fstream = fopen(argv[1], "r");
+	if (fstream == NULL) {
+		fprintf(stderr, "Unable to open file '%s'\n", argv[1]);
+		exit(1); }
+
+	// Malloc a
+	a = (char *)malloc(size);
+	if (!a) {
+		perror("unable to allocate array a: "); 
+		exit(-1); }
+
+	// Read file to a
+	i = 0;
+	while (fscanf(fstream, "%s", stream) != EOF) {
+		if (debug)
+			fprintf(stderr, "%.30s\n", stream);
+		strcpy(&a[i], stream);
+		i += string_len;
+		num_strings++; }
+		
+	// Close file and stream
+	free(stream);
+	fclose(fstream);
+	
+	// Stop clock and record read time
+	end = MPI_Wtime();
+	wall_server = end - start;
+	
+	
+	
+	// Start clock
+	start = MPI_Wtime();
+	
+	// Malloc local copy of a (WIP)
+	int num_local_strings = num_strings / numprocs;
+	
+
+	// Copy relevant strings to local process (WIP)
+	
+	
+	// Stop clock and record copy time
+	end = MPI_Wtime();
+	wall_client = end - start;
+	
+	
+	
+	// Print wall times
+	if (debug) {
+		fprintf(stderr, "Tot. read/copy wall time: server = %f, clients = %f\n", wall_server, wall_client);
+		fprintf(stderr, "Avg. read/copy wall time: server = %f, clients = %f\n", wall_server / (double)num_strings, wall_client / (double)num_strings); }
+	
+	// Choose the first string in the list, compare it to all the other strings in the list
+	strncpy(target, &a[0], string_len);
+	int a_char_size = num_strings * string_len;
+	
+	
+	
+	// Start clock
+	start = MPI_Wtime();
+		
+	// The actual comparison (working in chunks with early exit)
+	num_comps = num_matches = 0;
+	for (i = string_len; i < a_char_size; i += string_len) {
+		strncpy(other, &a[i], string_len);
+		num_matches += is_match(target, other, string_len, threshold);
+		num_comps++; }
+
+	// Stop clock and record/print comp time
+	end = MPI_Wtime();
+	wall_comp = end - start;
+	if (debug) 
+		fprintf(stdout, "num_strings = %d, num_comps = %d, num_matches = %d, wall_comp = %.6f\n", num_strings, num_comps, num_matches, wall_comp);	
 	
 	// Cleanup
 	free(a); free(local_a);
