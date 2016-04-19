fde6d8ae865542d6392cd4d78852ffeea16ee6d8
Byron Roosa <violinuxer@gmail.com>
2016-04-19 15:51:12 -0400

    Added inline asm is_match

--- main.c
+++ main.c
@@ -3,41 +3,26 @@
 #include <mpi.h>
 #include <stdbool.h>
 
-bool is_match(char *a, char *b, int len, int max)
-{
-	int diff = 0;
-	for (int j = 0; j < len; j += 6)
-	{
-		diff += (a[j    ] != b[j    ]);
-		diff += (a[j + 1] != b[j + 1]);
-		diff += (a[j + 2] != b[j + 2]);
-		diff += (a[j + 3] != b[j + 3]);
-		diff += (a[j + 4] != b[j + 4]);
-		diff += (a[j + 5] != b[j + 5]);
-		if (diff > max)
-			return false;
-	}
-	return true;
-}
+bool is_match(char *a, char *b, int len, int max);
 
 int main(int argc, char *argv)
 {
 	// MPI variables
-	int numprocs, myrank, namelen;	
+	int numprocs, myrank, namelen;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Status status;
-	
+
 	// Program variables
 	bool debug = true;
 	int i, j; // counters
 	double start = 0.0, end = 0.0, wall_server, wall_client, wall_comp;
-	
+
 	// Main variables
 	int string_len, max_num_strings = 1e6, num_strings = 0, threshold = 2;
 	int diff, distance, local_distance, num_comps;
 	char *a, *local_a, *stream;
 	FILE *fstream = NULL;
-	
+
 	// Initialize MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -47,17 +32,36 @@ int main(int argc, char *argv)
 	// Setup input
 	if (argc < 3) {
 		fprintf(stderr, "Usage: ./main file-name string-length\n");
-		exit(1); }
+		exit(1);
+    }
 	string_len = atoi(argv[2]);
-	
+
 	// Variables dependent on string length
 	int array_len = string_len + 1;
 	size_t size = sizeof(char) * max_num_strings * array_len;
 	stream = (char *)malloc(sizeof(char) * array_len);
 	char target[array_len], other[array_len];
-	
+
 	// Cleanup
 	free(a); free(local_a);
 	MPI_Finalize();
 	exit(0);
+}
+
+bool is_match(char *a, char *b, int len, int max)
+{
+    for (int i = 0; i < len; i++) {
+        char a_char = *(a + i);
+        char b_char = *(b + i);
+
+        char is_match;
+        asm("mov %1, %al\n\t"
+            "xor %2, %al\n\t"
+            "sub 1, al\n\t"
+            "rol 1, al\n\t"
+            "and 1, al\n\t"
+            : "=r" (is_match))
+            : "r" (a_char), "r" (b_char)
+            : "cc")
+    }
 }
