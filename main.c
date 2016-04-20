0e48caf65d2dc5218af4cac891c7cc77c79f0d88
Byron Roosa <violinuxer@gmail.com>
2016-04-20 18:09:31 -0400

    Added asm string compare for fun

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+//#include <mpi.h>
 #include <stdbool.h>
 
 bool is_match(char *a, char *b, int len, int max);
@@ -50,18 +50,49 @@ int main(int argc, char *argv)
 
 bool is_match(char *a, char *b, int len, int max)
 {
+
+    int diff_count = 0;
     for (int i = 0; i < len; i++) {
         char a_char = *(a + i);
         char b_char = *(b + i);
 
         char is_match;
-        asm("mov %1, %al\n\t"
-            "xor %2, %al\n\t"
-            "sub 1, al\n\t"
-            "rol 1, al\n\t"
-            "and 1, al\n\t"
-            : "=r" (is_match))
+
+        //Check whether the twho characters match, but don't
+        //use a branch. ASM is needed to do a left rotate
+        //so that we can interpret the sign bit seperately.
+
+        //If a == b, then a ^ b == 0. We then subtract one and
+        //rotate the byte left by 1 bit so that the sign bit
+        //can be masked off. The result will be 0 if the characters
+        //match and 1 if they don't. We can then maintain a
+        //running count of the distance between the two strings
+        //without using a branch. This is probably unnecessary, but
+        //is way too cool to do without.
+
+        //NOTE: this only works if a and b are less than 128.
+        //Otherwise the sign bits will
+        asm ("mov %1, %%al\n\t"
+            "xor %2, %%al\n\t"
+            "sub $1, %%al\n\t"
+            "rol $1, %%al\n\t"
+            "and $0x01, %%al\n\t"
+            "xor $0x01, %%al\n\t"
+            "mov %%al, %0"
+            : "=r" (is_match)
             : "r" (a_char), "r" (b_char)
-            : "cc")
+            : "%al");
+
+        printf("%d\n", is_match);
+
+        //is_match will be 0 when the chars are equal and will
+        //be one when they are different
+        diff_count += is_match;
+
+        if (diff_count > max) {
+            return false;
+        }
     }
+
+    return true;
 }
