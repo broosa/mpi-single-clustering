eea50a89cbb0a7a0708124498ab4f1b51a2a5845
LiberalOtaku <ledgam3r1279@gmail.com>
2016-04-22 15:06:51 -0400

    More comments and Byron's assembly implementation
    while also working in chunks

--- main.c
+++ main.c
@@ -1,30 +1,67 @@
 // File: main.c
 // Authors: Edward Ly, Byron Roosa(, and possibly George Crowson)
-// Last Updated: 19 April 2016
+// Last Updated: 22 April 2016
 // Single-linkage clustering program on strings using MPI implementation.
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <stdbool.h>
 
+// The individual character comparison
+int is_match_char(char a, char b)
+{
+	char is_matched;
+	
+	//Check whether the twho characters match, but don't
+	//use a branch. ASM is needed to do a left rotate
+	//so that we can interpret the sign bit separately.
+
+	//If a == b, then a ^ b == 0. We then subtract one and
+	//rotate the byte left by 1 bit so that the sign bit
+	//can be masked off. The result will be 0 if the characters
+	//match and 1 if they don't. We can then maintain a
+	//running count of the distance between the two strings
+	//without using a branch. This is probably unnecessary, but
+	//is way too cool to do without.
+
+	//NOTE: this only works if a and b are less than 128.
+	//Otherwise the sign bits will
+	asm ("mov %1, %%al\n\t"
+		"xor %2, %%al\n\t"
+		"sub $1, %%al\n\t"
+		"rol $1, %%al\n\t"
+		"and $0x01, %%al\n\t"
+		"xor $0x01, %%al\n\t"
+		"mov %%al, %0"
+		: "=r" (is_matched)
+		: "r" (a), "r" (b)
+		: "%al");
+
+	//is_match will be 0 when the chars are equal and will
+	//be one when they are different
+	
+	return (int)is_matched;
+}
+
+// The actual comparison (working in chunks with early exit)
 bool is_match(char *a, char *b, int len, int max)
 {
 	int diff = 0;
-	for (int j = 0; j < len; j += 6)
+	for (int i = 0; i < len; i += 6)
 	{
-		diff += (a[j    ] != b[j    ]);
-		diff += (a[j + 1] != b[j + 1]);
-		diff += (a[j + 2] != b[j + 2]);
-		diff += (a[j + 3] != b[j + 3]);
-		diff += (a[j + 4] != b[j + 4]);
-		diff += (a[j + 5] != b[j + 5]);
+		diff += is_match_char(*(a + i    ), *(b + i    ));
+		diff += is_match_char(*(a + i + 1), *(b + i + 1));
+		diff += is_match_char(*(a + i + 2), *(b + i + 2));
+		diff += is_match_char(*(a + i + 3), *(b + i + 3));
+		diff += is_match_char(*(a + i + 4), *(b + i + 4));
+		diff += is_match_char(*(a + i + 5), *(b + i + 5));
 		if (diff > max)
 			return false;
 	}
 	return true;
 }
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	// MPI variables
 	int numprocs, myrank, namelen;	
@@ -50,7 +87,7 @@ int main(int argc, char *argv)
 
 	// Setup input
 	if (argc < 3) {
-		fprintf(stderr, "Usage: ./main file-name string-length\n");
+		fprintf(stderr, "Usage: %s file-name string-length\n", argv[0]);
 		exit(1); }
 	string_len = atoi(argv[2]);
 	
@@ -124,7 +161,7 @@ int main(int argc, char *argv)
 	// Start clock
 	start = MPI_Wtime();
 		
-	// The actual comparison (working in chunks with early exit)
+	// Do the actual comparison (find the number of strings to merge)
 	num_comps = num_matches = 0;
 	for (i = string_len; i < a_char_size; i += string_len) {
 		strncpy(other, &a[i], string_len);
